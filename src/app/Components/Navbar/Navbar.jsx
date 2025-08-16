0e18b094be71eb8bade726a4202fdd10e1d4d4b0
Mukesh1346 <mukeshmahar00@gmail.com>
2025-08-16 11:50:23 +0530

    color change

--- src/app/Components/Navbar/Navbar.jsx
+++ src/app/Components/Navbar/Navbar.jsx
@@ -1,19 +1,25 @@
+"use client";
 import React from "react";
 import "./navbar.css";
-import logo from '@/app/Images/logo.webp'
+import logo from "@/app/Images/logo.webp";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Navbar() {
   return (
-  <>
-    <nav className="navbar navbar-expand-lg navbar-light  shadow-sm">
+    <nav className="navbar navbar-expand-lg navbar-light shadow-sm">
       <div className="container-fluid">
         {/* Logo */}
-        <a className="navbar-brand d-flex align-items-center" href="#">
-          <Image src={logo} alt="Logo" className="navbar-logo me-2" />
+        <Link href="/" className="navbar-brand d-flex align-items-center">
+          <Image
+            src={logo}
+            alt="Ocean Tech Logo"
+            width={40}
+            height={40}
+            className="navbar-logo me-2"
+          />
           {/* <span className="fw-bold brand-text">OCEAN TECH</span> */}
-        </a>
+        </Link>
 
         {/* Toggle Button */}
         <button
@@ -21,6 +27,9 @@ export default function Navbar() {
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -28,110 +37,78 @@ export default function Navbar() {
         {/* Menu */}
         <div className="collapse navbar-collapse" id="navbarNavDropdown">
           <ul className="navbar-nav mx-auto align-items-center">
-
             {/* Static Links */}
             <li className="nav-item">
-              <a className="nav-link" href="/">Home</a>
+              <Link href="/" className="nav-link">
+                Home
+              </Link>
             </li>
             <li className="nav-item">
-            <Link href="/Pages/about" className="nav-link">About Us</Link>
+              <Link href="/Pages/about" className="nav-link">
+                About Us
+              </Link>
             </li>
             <li className="nav-item">
-            <Link href="/Pages/bulk-sms" className="nav-link">Bulk SMS</Link>
+              <Link href="/Pages/bulk-sms" className="nav-link">
+                Bulk SMS
+              </Link>
             </li>
-              
             <li className="nav-item">
-            <Link href="/Pages/bulk-sms" className="nav-link">OTP SMS Service</Link>
+              <Link href="/Pages/otp-sms" className="nav-link">
+                OTP SMS Service
+              </Link>
             </li>
-
-
 
             {/* Products Dropdown */}
-            <li className="nav-item dropdown position-static">
-              <a className="nav-link dropdown-toggle" href="#">
+            <li className="nav-item  position-static">
+              <a
+                className="nav-link "
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
                 <i className="bi bi-box"></i> Products
               </a>
-              <div className="dropdown-menu mega-menu">
+              {/* <div className="dropdown-menu mega-menu shadow">
                 <div className="row p-4">
                   <div className="col-md-3">
                     <h6 className="fw-bold">Messaging</h6>
                     <ul className="list-unstyled">
-                      <li>Bulk SMS Promotion</li>
-                      <li>Bulk SMS Transaction</li>
-                      <li>OTP SMS</li>
-                      <li>International SMS</li>
-                      <li>WhatsApp SMS</li>
+                      <li><Link href="/Pages/bulk-sms">Bulk SMS Promotion</Link></li>
+                      <li><Link href="/Pages/sms-transaction">Bulk SMS Transaction</Link></li>
+                      <li><Link href="/Pages/otp-sms">OTP SMS</Link></li>
+                      <li><Link href="/Pages/international-sms">International SMS</Link></li>
+                      <li><Link href="/Pages/whatsapp-sms">WhatsApp SMS</Link></li>
                     </ul>
                   </div>
                   <div className="col-md-3">
                     <h6 className="fw-bold">Other Services</h6>
                     <ul className="list-unstyled">
-                      <li>Mobile Database</li>
-                      <li>Website Designing</li>
-                      <li>IVR Services</li>
-                      <li>Voice Call</li>
-                      <li>Email Marketing</li>
+                      <li><Link href="/Pages/mobile-database">Mobile Database</Link></li>
+                      <li><Link href="/Pages/website-design">Website Designing</Link></li>
+                      <li><Link href="/Pages/ivr">IVR Services</Link></li>
+                      <li><Link href="/Pages/voice-call">Voice Call</Link></li>
+                      <li><Link href="/Pages/email-marketing">Email Marketing</Link></li>
                     </ul>
                   </div>
                 </div>
-              </div>
+              </div> */}
             </li>
 
-            {/* Solutions Dropdown */}
-            <li className="nav-item dropdown position-static">
-              <a className="nav-link dropdown-toggle" href="#">
-                <i className="bi bi-lightbulb"></i> Solutions
-              </a>
-              <div className="dropdown-menu mega-menu">
-                <div className="row p-4">
-                  <div className="col-md-3">
-                    <h6 className="fw-bold">Digital Marketing</h6>
-                    <ul className="list-unstyled">
-                      <li>Search Engine Optimization</li>
-                      <li>Social Media Optimization</li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </li>
-
-            {/* Services (Normal Link) */}
+            {/* Contact */}
             <li className="nav-item">
-              <a className="nav-link" href="#">Services</a>
-            </li>
-
-            {/* Blog (Normal Link) */}
-            <li className="nav-item">
-              <a className="nav-link" href="#">Blog</a>
-            </li>
-
-            {/* Company Dropdown */}
-            <li className="nav-item dropdown position-static">
-              <a className="nav-link dropdown-toggle" href="#">
-                <i className="bi bi-building"></i> Company
-              </a>
-              <div className="dropdown-menu mega-menu">
-                <div className="row p-4">
-                  <div className="col-md-3">
-                    <h6 className="fw-bold">Policy Info</h6>
-                    <ul className="list-unstyled">
-                      <li>Privacy Policy</li>
-                      <li>Terms & Conditions</li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </li>
-
-            {/* Contact (Normal Link) */}
-            <li className="nav-item">
-              <a className="nav-link" href="#">Contact</a>
+              <Link href="/Pages/contact" className="nav-link">
+                Contact
+              </Link>
             </li>
           </ul>
 
           {/* Buttons */}
           <div className="d-flex align-items-center">
-            <button className="btn btn-outline-dark me-2">Get Started</button>
+            <Link href="/Pages/get-started" className="btn btn-outline-dark me-2">
+              Get Started
+            </Link>
             <a href="tel:+919868629924" className="btn btn-primary">
               <i className="bi bi-telephone"></i> +91-9868629924
             </a>
@@ -139,7 +116,5 @@ export default function Navbar() {
         </div>
       </div>
     </nav>
-  
-  </>
   );
 }
